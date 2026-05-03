047f4aa5789408fe77c5b8c490ab663cac062e9c
Krish Sawant <krish.sawant@Krishs-MacBook-Air.local>
2026-05-04 01:17:52 +0530

    changes in students and attendence apps

--- attendance_system/attendance/admin.py
+++ attendance_system/attendance/admin.py
@@ -1,16 +1,54 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Attendance, AttendanceSession
+
+
+STATUS_COLORS = {
+    'P': 'green',
+    'A': 'red',
+    'L': 'orange',
+    'E': 'blue',
+}
 
 
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ('student', 'class_ref', 'date', 'status', 'marked_by')
-    list_filter  = ('status', 'date', 'class_ref')
-    search_fields = ('student__first_name', 'student__last_name', 'student__student_id')
+    list_display   = (
+        'student', 'class_ref', 'date',
+        'colored_status', 'marked_by', 'remarks'
+    )
+    list_filter    = ('status', 'date', 'class_ref')
+    search_fields  = (
+        'student__first_name', 'student__last_name',
+        'student__student_id'
+    )
     date_hierarchy = 'date'
+    ordering       = ('-date',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    def colored_status(self, obj):
+        color = STATUS_COLORS.get(obj.status, 'black')
+        return format_html(
+            '<span style="color:{}; font-weight:bold;">{}</span>',
+            color, obj.get_status_display()
+        )
+    colored_status.short_description = 'Status'
 
 
 @admin.register(AttendanceSession)
 class AttendanceSessionAdmin(admin.ModelAdmin):
-    list_display = ('class_ref', 'date', 'marked_by', 'is_finalized', 'created_at')
-    list_filter  = ('is_finalized', 'class_ref')
+    list_display  = (
+        'class_ref', 'date', 'marked_by',
+        'finalized_badge', 'created_at'
+    )
+    list_filter   = ('is_finalized', 'class_ref')
+    search_fields = ('class_ref__name',)
+    ordering      = ('-date',)
+
+    def finalized_badge(self, obj):
+        color = 'green' if obj.is_finalized else 'orange'
+        label = 'Finalized' if obj.is_finalized else 'Pending'
+        return format_html(
+            '<span style="color:{}; font-weight:bold;">{}</span>', color, label
+        )
+    finalized_badge.short_description = 'Session Status'
--- attendance_system/students/admin.py
+++ attendance_system/students/admin.py
@@ -1,16 +1,56 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Student, Class
 
 
 @admin.register(Class)
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ('name', 'section', 'teacher', 'created_at')
-    list_filter = ('name',)
-    search_fields = ('name', 'section')
+    list_display  = ('name', 'section', 'teacher', 'student_count', 'created_at')
+    list_filter   = ('name',)
+    search_fields = ('name', 'section', 'teacher__username')
+    ordering      = ('name', 'section')
+
+    def student_count(self, obj):
+        count = obj.students.filter(is_active=True).count()
+        return format_html('<b>{}</b>', count)
+    student_count.short_description = 'Active Students'
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'full_name', 'gender', 'assigned_class', 'is_active')
-    list_filter = ('gender', 'assigned_class', 'is_active')
-    search_fields = ('student_id', 'first_name', 'last_name', 'email')
+    list_display   = (
+        'student_id', 'full_name', 'gender',
+        'assigned_class', 'guardian_phone', 'status_badge'
+    )
+    list_filter    = ('gender', 'assigned_class', 'is_active')
+    search_fields  = ('student_id', 'first_name', 'last_name', 'email')
+    ordering       = ('first_name', 'last_name')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('Basic Info', {
+            'fields': (
+                'student_id', 'first_name', 'last_name',
+                'gender', 'date_of_birth', 'profile_picture'
+            )
+        }),
+        ('Contact', {
+            'fields': ('email', 'phone', 'address')
+        }),
+        ('Class & Guardian', {
+            'fields': (
+                'assigned_class', 'guardian_name', 'guardian_phone'
+            )
+        }),
+        ('Status', {
+            'fields': ('is_active', 'created_at', 'updated_at')
+        }),
+    )
+
+    def status_badge(self, obj):
+        color = 'green' if obj.is_active else 'red'
+        label = 'Active' if obj.is_active else 'Inactive'
+        return format_html(
+            '<span style="color:{}; font-weight:bold;">{}</span>', color, label
+        )
+    status_badge.short_description = 'Status'
